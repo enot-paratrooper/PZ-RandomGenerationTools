f0277259c8e23526af3b3d6c2ada293360b34968
Kiresenooj <serg331962@yandex.ru>
2026-08-17 19:15:42 +0300

    Revert "Initial Commit"

--- src/randRoomGen/ObjectTiles/InitializeTile.java
+++
@@ -1,6 +0,0 @@
-package randRoomGen.ObjectTiles;
-import org.w3c.dom.Element;
-
-public interface InitializeTile {
-	void initialize(Element element);
-}
--- src/tools/linker/LinkUnit.java
+++
@@ -1,7 +0,0 @@
-package tools.linker;
-
-public class LinkUnit {
-	public String type;
-	public String Name;
-	public int reference;
-}
